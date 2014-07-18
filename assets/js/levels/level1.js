436dfa2cd137dd227d878ec0c3b65e9a4a92b578
bloomspin <contact@bloomspin.com>
2014-07-17 21:06:19 -0400

    Add Building1 Mesh

--- assets/js/levels/level1.js
+++ assets/js/levels/level1.js
@@ -3,7 +3,7 @@ var level = new Object();
 
 //INIT
 function levelInit(){
-	console.log("init");
+
 	//add a create
   var crateSize = 5;
   meshes.crate = basicCrate(crateSize);
@@ -14,6 +14,18 @@ function levelInit(){
   meshes.crate = basicCrate(crateSize);
   meshes.crate.position.set(20, crateSize/2, 0);
   scene.add( meshes.crate);
+	
+	var loader = new THREE.JSONLoader();
+  loader.load('./mesh/building1.js', function (geometry) {
+			// create a new material
+			  meshes.building1 = new THREE.Mesh(
+				geometry
+			  );
+			  meshes.building1.scale.set(10,10,10);
+			  meshes.building1.position.set(10, 0, 30);
+			  scene.add(meshes.building1);
+			  		  
+	});		
 	
 
 };
